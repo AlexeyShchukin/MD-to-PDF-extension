c377732e64060563e4c7370650316d055413c2cc
AlexeyShchukin <shchukin.developer@gmail.com>
2026-01-01 08:00:48 +0100

    add welcome and uninstall pages

--- background.js
+++ background.js
@@ -1,3 +1,26 @@
 chrome.action.onClicked.addListener(async () => {
   await chrome.tabs.create({ url: "https://md2pdf.dev/" });
 });
+
+const UNINSTALL_URL = "https://md2pdf.dev/uninstall/";
+chrome.runtime.setUninstallURL(UNINSTALL_URL);
+
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+    // Code to be executed on first install
+    // eg. open a tab with a url
+    chrome.tabs.create({
+      url: "https://md2pdf.dev/welcome/",
+    });
+  } else if (details.reason === chrome.runtime.OnInstalledReason.UPDATE) {
+    // When extension is updated
+  } else if (
+    details.reason === chrome.runtime.OnInstalledReason.CHROME_UPDATE
+  ) {
+    // When browser is updated
+  } else if (
+    details.reason === chrome.runtime.OnInstalledReason.SHARED_MODULE_UPDATE
+  ) {
+    // When a shared module is updated
+  }
+});
